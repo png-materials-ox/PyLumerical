5b27be48325fa56116537b0acc1daf176b58bdb3
png-materials-ox <png.materials.ox@gmail.com>
2024-07-28 12:24:51 +0100

    uno mas

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,8 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sphinx_rtd_theme
+
 project = 'PyLumerical'
 copyright = '2024, Gareth Sion Jones'
 author = 'Gareth Sion Jones'
@@ -30,4 +32,4 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-#html_static_path = ['_static']
+html_static_path = ['_static']
