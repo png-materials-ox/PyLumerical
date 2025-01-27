2daedf77b8b62a0f8887b2c734044ee681d8ed7c
Gareth Jones <garetrhsion@googlemail.com>
2025-01-27 18:49:17 +0000

    can't get rtd theme to work

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,7 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx_rtd_theme'
+    'sphinx.ext.autosummary'
 ]
 
 templates_path = ['_templates']
@@ -30,5 +29,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'classic'
 html_static_path = ['_static']
